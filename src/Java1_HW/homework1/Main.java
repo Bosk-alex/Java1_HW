5df92b576fe2d01be8fa7a26cca2144d18430d33
Alex Filippov <filippov.pda@gmail.com>
2020-02-10 11:02:46 +0400

    Initial commit

---
+++ src/Java1_HW/homework1/Main.java
@@ -0,0 +1,43 @@
+package Java1_HW.homework1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static Java1_HW.homework1.TaskMethods.*;
+
+public class Main {
+
+    public static void main(String... args) {
+/*
+        System.out.println(args.getClass());
+        String[] array = {"1", "3", "4"};
+        String[] newArray = Arrays.copyOf(array, 15);
+        Arrays.fill(newArray, array.length, newArray.length, "#");
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(newArray).replaceAll("[\\[, \\]]", ""));
+
+        int a = 1;
+        ArrayList list = new ArrayList();
+        list.add(a);
+        a = (int) list.get(0);
+        String s = "10";
+        Integer.valueOf(s);
+*/
+        task1();
+        task2();
+        int c = 4;
+        System.out.println(c + "ddd");
+    }
+
+ }
+
+class TaskMethods {
+    static void task1() {
+        System.out.println(1);
+    }
+
+    static void task2() {
+        System.out.println(2);
+    }
+}
+
